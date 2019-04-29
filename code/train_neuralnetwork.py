307b6b7159c1f3554be52c68b12420cd5f64f25a
raulbarrue <raulbarrue@outlook.com>
2019-04-29 23:35:36 +0100

    Created NN, but doesn't work. Predicts all 0

--- code/train_neuralnetwork.py
+++ code/train_neuralnetwork.py
@@ -4,6 +4,9 @@ from keras.models import Sequential
 from keras.layers import Dense
 from sklearn.model_selection import train_test_split
 
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+
 
 #### UNBALANCED DATASET ### 
 
@@ -12,6 +15,7 @@ numpy.random.seed(7)
 
 # Loading data into a pandas dataframe
 dataset = pd.read_csv("data/train.csv")
+dataset = dataset.drop("ID_code", axis = 1)
 
 # Split train / test
 train, test = train_test_split(dataset, test_size=0.2)
@@ -26,17 +30,17 @@ y_test = test["target"].values
 
 
 # Neural Network
-n_cols = len(train.columns)
+n_cols = len(train.columns) - 1 #removes the target column
 model = Sequential()
 
 # NN Structure
 model.add(Dense(100, activation = "relu", input_shape = (n_cols,), name = "dense1"))
 model.add(Dense(100, activation = "relu", name = "dense2"))
 model.add(Dense(100, activation = "relu", name = "dense3"))
-model.add(Dense(10, activation = "softmax", name = "dense4"))
+model.add(Dense(1, activation = "sigmoid", name = "dense4"))
 
 # NN Compiler
 model.compile(optimizer = "adam", loss = "binary_crossentropy", metrics = ["accuracy"])
 
 # NN Fit
-history = model.fit(X_train, y_train, epochs = 150, )
+history = model.fit(X_train, y_train, epochs = 20, validation_data = (X_test, y_test))
