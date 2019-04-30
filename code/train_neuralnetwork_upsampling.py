3080f874a5d66b10d532cd025e486617a008dbae
raulbarrue <raulbarrue@outlook.com>
2019-04-30 20:49:46 +0100

    Fixed stuff

--- code/train_neuralnetwork_upsampling.py
+++ code/train_neuralnetwork_upsampling.py
@@ -4,6 +4,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+from sklearn.utils import resample
 
 # User outputs
 plot_results = False
@@ -37,8 +38,22 @@ numpy.random.seed(7)
 train_dataset = pd.read_csv("data/train.csv")
 train_dataset = train_dataset.drop("ID_code", axis = 1)
 
+# Separate majority and minority classes
+traindf_majority = train_dataset[train_dataset.target==0]
+traindf_minority = train_dataset[train_dataset.target==1]
+
+# Upsample minority class
+df_minority_upsampled = resample(traindf_minority, 
+                                 replace=True,     # sample with replacement
+                                 n_samples=len(traindf_majority),    # to match majority class
+                                 random_state=123) # reproducible results
+
+# Combine majority class with upsampled minority class
+df_upsampled = pd.concat([traindf_majority, df_minority_upsampled])
+
+
 # Split train / test
-train, test = train_test_split(train_dataset, test_size=0.2)
+train, test = train_test_split(df_upsampled, test_size=0.2)
 
 # Splitting between features and target
 X_train = train.drop("target", axis = 1).values
